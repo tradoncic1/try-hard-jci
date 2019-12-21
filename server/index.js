56a10d064f6af9f2f93f2b23fc039f69db4464df
Hamdija <hr@freund.ba>
2019-12-21 11:05:55 +0100

    pull me

--- server/index.js
+++ server/index.js
@@ -1,1 +1,8 @@
 const mongojs = require('mongojs')
+const express = require('express')
+const jwt = require('json-web-token')
+const bodyParser = require('body-parser')
+const PORT = 4200;
+
+
+app.use(bodyParser.json())
