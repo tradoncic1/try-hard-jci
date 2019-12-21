02bf03ed9ff69018531ac6e8449c631c16c330d0
tradoncic1 <tarik.r65@gmail.com>
2019-12-21 11:02:22 +0100

    install dependencies

---
+++ client/src/pages/login/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Form, FormGroup, Input } from "reactstrap";
+
+import "./Login.css";
+
+const Login = () => {
+  return (
+    <div className="Login-Wrap">
+      <Form></Form>
+    </div>
+  );
+};
+
+export default Login;
